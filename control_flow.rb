4046b61caab7476bdd43237c4361d0693618280b
Trevor Vardeman <trevortx@gmail.com>
2022-06-12 00:38:06 -0700

    commit tree 00f8de4e56efeb143ff6709045fac1db41860b6d

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,40 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if username.upcase == "ADMIN" && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 then "It's brisk out there!"
+  elsif temperature > 39 && temperature < 66 then "It's a little chilly out there!"
+  elsif temperature > 85 then "It's too dang hot out there!"
+  else "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 5 == 0 && num % 3 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+    when operation = "+" then num1 + num2
+    when operation = "-" then num1 - num2
+    when operation = "*" then num1 * num2
+    when operation = "/" then num1 / num2
+    else puts "Invalid operation!"
+    nil
+    end
 end
 
